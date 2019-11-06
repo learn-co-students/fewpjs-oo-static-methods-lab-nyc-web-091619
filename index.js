7eb31702375cee984cee7b61741c00ae8d7fe326
H. Turcan Cicek <htcicek@users.noreply.github.com>
2019-11-06 14:05:43 -0500

    met all specs

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^\w\s-']+/g, '');
+  }
+
+  static titleize(string) {
+    const excluded = [
+      'a',
+      'an',
+      'but',
+      'of',
+      'and',
+      'for',
+      'at',
+      'by',
+      'from',
+      'the',
+    ];
+    let newStringArr = (
+      string[0].toUpperCase() + string.slice(1).toLowerCase()
+    ).split(' ');
+
+    return newStringArr
+      .map(word => {
+        if (excluded.includes(word)) return word;
+        return word[0].toUpperCase() + word.slice(1);
+      })
+      .join(' ');
+  }
 }
