5d25547512ab46918628611dcadf54fc0bb82cdd
VPoshelyuk <aslava198@gmail.com>
2019-11-06 20:54:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9 '-]/g, '' )
+  }
+
+  static titleize(string){
+    const noGo = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    
+    let arrayOfStrings = this.capitalize(string).split(" ")
+
+    return arrayOfStrings.map((word) => {
+      if (noGo.includes(word)){
+        return word
+      } else {
+        return this.capitalize(word)
+      }    
+    }).join(" ")
+  }
 }
